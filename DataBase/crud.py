24142ced25d4f12eacc975a54ebb2f223eab4693
ibras0696 <ibras0696>
2025-03-16 01:32:57 +0300

    Удаление таблиц с Vip

--- DataBase/crud.py
+++ DataBase/crud.py
@@ -7,7 +7,7 @@ from datetime import datetime
 Users_table = 'Users_table'
 Search_table = 'Search_table'
 Order_table = 'Order_table'
-Vip_table = 'Vip_table'
+
 
 BASE_NAME = './DataBase/Data_Base_WB.db'
 
@@ -24,7 +24,6 @@ async def create_table():
             telegram_name Имя Пользователя
             free_search Количество бесплатных дней
             data_connect Дата Регистрации Пользователя
-            status Статус Пользователя Vip или User
         '''
         cur.execute(f'''
         CREATE TABLE IF NOT EXISTS {Users_table}(
@@ -32,8 +31,7 @@ async def create_table():
             telegram_id INTEGER PRIMARY KEY,
             telegram_name TEXT,
             free_search INTEGER,
-            data_connect TEXT,
-            status TEXT
+            data_connect TEXT
         )
         ''')
 
@@ -57,63 +55,16 @@ async def create_table():
         )
         ''')
 
-        # _____________________________________________________________
-        f'''
-        Таблица {Order_table}
-            id_transaction Айди транзакции
-            telegram_id Айди Телеграм
-            buy_total Общая потраченная сумма Пользователём
-            check_id Айди Чека
-            data_transaction Дата транзакции
-            FOREIGN KEY (telegram_id) REFERENCES {Users_table}(telegram_id) Связка Айди Телеграмма
-        '''
-        cur.execute(f'''
-        CREATE TABLE IF NOT EXISTS {Order_table}(
-            id_transaction INTEGER AUTO_INCREMENT,
-            telegram_id INTEGER PRIMARY KEY,
-            buy_total TEXT,
-            check_id TEXT, 
-            data_transaction TEXT,
-            
-            FOREIGN KEY (telegram_id) REFERENCES {Users_table}(telegram_id)
-        )
-        ''')
 
-        # _____________________________________________________________
-        f'''
-        Таблица {Vip_table}
-            id_vip_user Айди Вип Пользователя
-            telegram_id Айди Телеграм
-            buy_total Общая потраченная сумма Вип Пользователя
-            search_total Общее количество сделанных поисков
-            data_register_vip Дата регистрации вип пользователя
-            status TEXT Статус пользователя
-            FOREIGN KEY (status) REFERENCES {Users_table}(status) Связка статуса Пользователя и Вип пользователя
-            FOREIGN KEY (telegram_id) REFERENCES {Users_table}(telegram_id) Связка Айди Телеграмма
-        '''
-        cur.execute(f'''
-        CREATE TABLE IF NOT EXISTS {Vip_table}(
-            id_vip_user INTEGER AUTO_INCREMENT,
-            telegram_id INTEGER PRIMARY KEY,
-            buy_total INTEGER,
-            search_total INTEGER,
-            data_register_vip TEXT,
-            status TEXT,
-            
-            FOREIGN KEY (status) REFERENCES {Users_table}(status)
-            FOREIGN KEY (telegram_id) REFERENCES {Users_table}(telegram_id)
-        )
-        ''')
 
         conn.commit()
 
 # Функции для работы с таблицей Users_table
-async def register_user_in_table(telegram_id: int, telegram_name: str | None,  free_search: int=10) -> bool:
+async def register_user_in_table(telegram_id: int, telegram_name: str | None) -> bool:
     '''
-    Функция регистрирует пользователя и выдаёт 10 бесплатных попыток если уже есть то просто возвращает True
+    Функция регистрирует пользователя
     :param telegram_id: Айди телеграмма
     :param telegram_name: Имя Телеграмма
-    :param free_search: Количество бесплатных попыток
     :return: bool
     '''
     with sqlite3.connect(BASE_NAME) as conn:
@@ -121,8 +72,6 @@ async def register_user_in_table(telegram_id: int, telegram_name: str | None,  f
         current_date = datetime.now()
         # Форматируем текущую дату в строку
         data_connect = current_date.strftime("%d.%m.%Y")
-        # Статус пользователя
-        status = 'User'
         cur = conn.cursor()
         items = cur.execute(f"SELECT telegram_id FROM {Users_table} WHERE telegram_id == ?", (telegram_id, )).fetchall()
 
@@ -130,11 +79,12 @@ async def register_user_in_table(telegram_id: int, telegram_name: str | None,  f
             return True
         else:
             cur.execute(f'''
-            INSERT INTO {Users_table}(telegram_id, telegram_name, free_search, data_connect, status) 
-            VALUES (?, ?, ?, ?, ?)
-            ''', (telegram_id, f'{telegram_name} ', free_search, data_connect, status))
+            INSERT INTO {Users_table}(telegram_id, telegram_name,  data_connect) 
+            VALUES (?, ?, ? )
+            ''', (telegram_id, f'{telegram_name} ',  data_connect))
             conn.commit()
             return False
+
 
 # Проверка существования пользователя Users_table
 async def checking_user_in_table(telegram_id: int) -> bool:
@@ -142,6 +92,7 @@ async def checking_user_in_table(telegram_id: int) -> bool:
         cur = conn.cursor()
         items = cur.execute(f"SELECT telegram_id FROM {Users_table} WHERE telegram_id == ?", (telegram_id,)).fetchall()
         return True if len(items) != 0 else False
+
 
 # Функция для получения данных с Users_table
 async def get_user_table():
